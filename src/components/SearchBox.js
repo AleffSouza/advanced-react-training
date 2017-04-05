4d3b82347664104c8a298b315a852c729864c6dc
turbobeast <georgemichaelfaust@gmail.com>
2017-04-05 17:14:45 -0400

    keeping track of search term

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const SearchBox = (props) => {
-  const { onSearchChange } = props;
+  const { onSearchChange, searchTerm } = props;
   return (
     <div className='pa2'>
       <input className='pa2'
         type='search'
         placeholder='search Robots...'
         onChange={onSearchChange}
+        value={searchTerm}
       />
     </div>
   );
