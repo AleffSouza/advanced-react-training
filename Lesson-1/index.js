0dea242b2c9f062ca845297eb23a49e51caf4487
turbobeast <georgemichaelfaust@gmail.com>
2017-04-11 17:03:00 -0400

    removed lessons dirs

--- Lesson-1/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // glue between React and the DOM
-import App from './Containers/App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
